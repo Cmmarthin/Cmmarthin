f2480b316e53ca1006958b7a5273014b415b1118
Cmmarthin <cmmartin063@gmail.com>
2025-06-03 11:32:28 -0500

    Update 3.py

--- ejercicios10py/3.py
+++ ejercicios10py/3.py
@@ -1,5 +1,7 @@
 #Ejercicio 3: Área de un triángulo
 #Fórmula: Área = (base * altura) / 2
-b = float(input())
-h = float(input())
-print((b * h) / 2)
+b = float (input("ingrese la base del triangulo"))
+h = float (input("ingrese la altura del triangulo"))
+
+area = (b * h)/2
+print(f"la base del triangulo es :{area}")
