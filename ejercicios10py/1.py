aafecbc708b0117efc7ce610b0cf182885aacf56
Cmmarthin <cmmartin063@gmail.com>
2025-06-03 11:33:34 -0500

    Update 1.py

--- ejercicios10py/1.py
+++ ejercicios10py/1.py
@@ -1,5 +1,5 @@
-#Ejercicio 1: Suma de dos números
-#Descripción: Pide dos números al usuario, almacénalos en variables y muestra su suma.
-a = float(input())
-b = float(input())
-print(a + b)
+# Ejercicio 1: Suma de dos números
+# Descripción: Pide dos números al usuario, almacénalos en variables y muestra su suma.
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+print(f"La suma es: {a + b}")
